8ae328c0efce85667ba2b1f43155e3eefcbbafa7
Clément Renaud <clement+git@clementrenaud.com>
2025-01-20 16:55:50 +0100

    lint fix networks

--- packages/networks/src/networks/ethereum.ts
+++ packages/networks/src/networks/ethereum.ts
@@ -1,23 +1,23 @@
 export const ethereum = {
+  arb: {
+    outbox: '0x0B9857ae2D4A3DBe74ffE1d7DF045bb7F96E4840',
+    rollup: '0x5eF0D09d1E6204141B4d37530808eD19f60FBa35',
+    routerGateway: '0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef',
+  },
   chainId: 1,
+  hyperlaneMailbox: '0xc005dc82818d67AF737725bD4bf75435d065D239',
+  isTestNet: false,
   name: 'Ethereum',
+  op: {
+    portalProxy: '0xbEb5Fc579115071764c7423A4f12eDde41f106Ed',
+  },
   slug: 'ethereum',
+  udt: '0x90DE74265a416e1393A450752175AED98fe11517',
   usdc: {
     domain: 0n,
     messenger: '0xBd3fa81B58Ba92a82136038B25aDec7066af3155',
-    transmitter: '0x0a992d191DEeC32aFe36203Ad87D7d289a738F81',
     token: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-  },
-  udt: '0x90DE74265a416e1393A450752175AED98fe11517',
-  hyperlaneMailbox: '0xc005dc82818d67AF737725bD4bf75435d065D239',
-  isTestNet: false,
-  arb: {
-    routerGateway: '0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef',
-    outbox: '0x0B9857ae2D4A3DBe74ffE1d7DF045bb7F96E4840',
-    rollup: '0x5eF0D09d1E6204141B4d37530808eD19f60FBa35',
-  },
-  op: {
-    portalProxy: '0xbEb5Fc579115071764c7423A4f12eDde41f106Ed',
+    transmitter: '0x0a992d191DEeC32aFe36203Ad87D7d289a738F81',
   },
   weth: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
 }
--- packages/networks/src/networks/optimism.ts
+++ packages/networks/src/networks/optimism.ts
@@ -1,15 +1,15 @@
 export const optimism = {
   chainId: 10,
+  hyperlaneMailbox: '0xd4C1905BB1D26BC93DAC913e13CaCC278CdCC80D',
+  isTestNet: false,
   name: 'OP Mainnet',
   slug: 'optimism',
+  udt: '0xc709c9116dBf29Da9c25041b13a07A0e68aC5d2D',
   usdc: {
     domain: 2n,
-    token: '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85',
     messenger: '0x2B4069517957735bE00ceE0fadAE88a26365528f',
+    token: '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85',
     transmitter: '0x4D41f22c5a0e5c74090899E5a8Fb597a8842b3e8',
   },
   weth: '0x4200000000000000000000000000000000000006',
-  isTestNet: false,
-  hyperlaneMailbox: '0xd4C1905BB1D26BC93DAC913e13CaCC278CdCC80D',
-  udt: '0xc709c9116dBf29Da9c25041b13a07A0e68aC5d2D',
 }
