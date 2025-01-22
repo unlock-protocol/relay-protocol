535e38696e1cee340ee4527f6f6748ff14496373
Clément Renaud <clement+git@clementrenaud.com>
2025-01-22 23:36:13 +0100

    update sol pragam

--- smart-contracts/contracts/BridgeProxy/CCTPBridgeProxy.sol
+++ smart-contracts/contracts/BridgeProxy/CCTPBridgeProxy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.28;
 
 import {ITokenMessenger} from "../interfaces/cctp/ITokenMessenger.sol";
 import {IMessageTransmitter} from "../interfaces/cctp/IMessageTransmitter.sol";
