e388f4df3fed17988033450e97a715654841b4df
Clément Renaud <clement+git@clementrenaud.com>
2025-01-24 10:38:48 +0100

    morpho pool test

---
+++ smart-contracts/test/RelayPool/morpho-yield-pool.ethereum.ts
@@ -0,0 +1,164 @@
+import { expect } from 'chai'
+import { ethers, ignition } from 'hardhat'
+import { networks } from '@relay-protocol/networks'
+import { IUSDC, ERC4626, RelayPool } from '../../typechain-types'
+import { getStataToken } from '@relay-protocol/helpers'
+import { mintUSDC } from '../utils/hardhat'
+import RelayPoolModule from '../../ignition/modules/RelayPoolModule'
+
+const {
+  usdc: { token: USDC },
+  weth,
+} = networks[1]
+
+const WETH_MORPHO_POOL = '0x78Fc2c2eD1A4cDb5402365934aE5648aDAd094d0'
+const USDC_MORPHO_POOL = '0xd63070114470f685b75B74D60EEc7c1113d33a3D'
+
+describe('RelayBridge: use Morpho yield pool (WETH)', () => {
+  let relayPool: RelayPool
+  let usdc: IUSDC
+  let morphoUsdcPool: ERC4626
+  let userAddress: string
+
+  before(async () => {
+    const { chainId } = await ethers.provider.getNetwork()
+    const [user] = await ethers.getSigners()
+    userAddress = await user.getAddress()
+
+    usdc = await ethers.getContractAt('IUSDC', USDC)
+    await mintUSDC(USDC, userAddress, ethers.parseUnits('1000', 6))
+
+    // get Aave pool static wrapper
+    morphoUsdcPool = await ethers.getContractAt('ERC4626', USDC_MORPHO_POOL)
+
+    // the static token (sata) contract forwards assets to the main v3 pool
+    const staticAavePoolGetter = await ethers.getContractAt(
+      ['function aToken() view returns(address)'],
+      await morphoUsdcPool.getAddress()
+    )
+
+    // deploy the pool
+    const parameters = {
+      RelayPool: {
+        hyperlaneMailbox: networks[1].hyperlaneMailbox,
+        asset: await usdc.getAddress(),
+        name: `${await usdc.name()} Relay Pool`,
+        symbol: `${await usdc.symbol()}-REL`,
+        origins: [],
+        thirdPartyPool: await morphoUsdcPool.getAddress(),
+        weth,
+      },
+    }
+    ;({ relayPool } = await ignition.deploy(RelayPoolModule, {
+      parameters,
+      deploymentId: `RelayPool-${parameters.RelayPool.symbol}-${chainId.toString()}`,
+    }))
+  })
+
+  it('should deposit tokens into the Aave V3 yield pool when a user deposits funds', async () => {
+    const [user] = await ethers.getSigners()
+    const amount = ethers.parseUnits('1', 6)
+    const userAddress = await user.getAddress()
+    const relayPoolAddress = await relayPool.getAddress()
+    const balanceThirdPartyPoolBefore = await usdc.balanceOf(USDC_MORPHO_POOL)
+
+    // Approved the Token to be spent by the RelayPool
+    await (await usdc.connect(user).approve(relayPoolAddress, amount)).wait()
+
+    // Deposit tokens to the RelayPool
+    await relayPool.connect(user).deposit(amount, userAddress)
+
+    // Check balances
+    expect(await usdc.balanceOf(relayPoolAddress)).to.equal(0)
+    expect(await usdc.balanceOf(USDC_MORPHO_POOL)).to.equal(
+      balanceThirdPartyPoolBefore + amount
+    )
+  })
+
+  it('should deposit tokens into the Aave V3 yield pool when a user mints shares', async () => {
+    const [user] = await ethers.getSigners()
+    const userAddress = await user.getAddress()
+    const relayPoolAddress = await relayPool.getAddress()
+    const balanceThirdPartyPoolBefore = await usdc.balanceOf(USDC_MORPHO_POOL)
+
+    const newShares = ethers.parseUnits('0.4', 6)
+
+    // Preview the mint to get the amount of tokens to be deposited
+    const amount = await relayPool.previewMint(newShares)
+
+    // Approve the Token to be spent by the RelayPool
+    await usdc.connect(user).approve(await relayPool.getAddress(), amount)
+
+    // Deposit tokens to the RelayPool
+    await relayPool.connect(user).mint(newShares, userAddress)
+
+    // Check balances
+    expect(await usdc.balanceOf(relayPoolAddress)).to.equal(0)
+    expect(await usdc.balanceOf(USDC_MORPHO_POOL)).to.equal(
+      balanceThirdPartyPoolBefore + amount
+    )
+  })
+
+  it('should withdraw tokens from the Aave V3 yield pool when withdrawing liquidity', async () => {
+    const [user] = await ethers.getSigners()
+    const amount = ethers.parseUnits('1', 6)
+    const userAddress = await user.getAddress()
+    const relayPoolAddress = await relayPool.getAddress()
+
+    const balanceThirdPartyPoolBefore = await usdc.balanceOf(USDC_MORPHO_POOL)
+    // Approved the Token to be spent by the RelayPool
+    await (await usdc.connect(user).approve(relayPoolAddress, amount)).wait()
+
+    // Deposit tokens to the RelayPool
+    await relayPool.connect(user).deposit(amount, userAddress)
+
+    // Check balances
+    expect(await usdc.balanceOf(relayPoolAddress)).to.equal(0)
+    expect(await usdc.balanceOf(USDC_MORPHO_POOL)).to.equal(
+      balanceThirdPartyPoolBefore + amount
+    )
+
+    // Withdraw tokens from the RelayPool
+    const withdrawAmount = ethers.parseUnits('0.5', 6)
+    await relayPool
+      .connect(user)
+      .withdraw(withdrawAmount, userAddress, userAddress)
+    expect(await usdc.balanceOf(relayPoolAddress)).to.equal(0)
+    expect(await usdc.balanceOf(USDC_MORPHO_POOL)).to.equal(
+      balanceThirdPartyPoolBefore + amount - withdrawAmount
+    )
+  })
+
+  it('should withdraw tokens from the Aave V3 yield pool when redeeming shares', async () => {
+    const [user] = await ethers.getSigners()
+    const amount = ethers.parseUnits('1', 6)
+    const userAddress = await user.getAddress()
+    const relayPoolAddress = await relayPool.getAddress()
+
+    const balanceThirdPartyPoolBefore = await usdc.balanceOf(USDC_MORPHO_POOL)
+    // Approved the Token to be spent by the RelayPool
+    await (await usdc.connect(user).approve(relayPoolAddress, amount)).wait()
+
+    // Deposit tokens to the RelayPool
+    await relayPool.connect(user).deposit(amount, userAddress)
+
+    // Check balances
+    expect(await usdc.balanceOf(relayPoolAddress)).to.equal(0)
+    expect(await usdc.balanceOf(USDC_MORPHO_POOL)).to.equal(
+      balanceThirdPartyPoolBefore + amount
+    )
+
+    // Withdraw tokens from the RelayPool
+    const sharesToRedeem = await relayPool.balanceOf(userAddress)
+    const amountToReceive = await relayPool.previewRedeem(sharesToRedeem)
+
+    await relayPool
+      .connect(user)
+      .redeem(sharesToRedeem, userAddress, userAddress)
+
+    expect(await usdc.balanceOf(relayPoolAddress)).to.equal(0)
+    expect(await usdc.balanceOf(USDC_MORPHO_POOL)).to.equal(
+      balanceThirdPartyPoolBefore + amount - amountToReceive
+    )
+  })
+})
