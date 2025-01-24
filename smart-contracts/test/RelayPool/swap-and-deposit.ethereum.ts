1bbe4f134247b885f2d042b616966cf2bc14d3d1
Clément Renaud <clement+git@clementrenaud.com>
2025-01-24 14:42:16 +0100

    add MORPHO test

--- smart-contracts/test/RelayPool/swap-and-deposit.ethereum.ts
+++ smart-contracts/test/RelayPool/swap-and-deposit.ethereum.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import { ethers, ignition } from 'hardhat'
 import networks from '@relay-protocol/networks'
 import RelayPoolModule from '../../ignition/modules/RelayPoolModule'
-import { mintUSDC } from '../utils/hardhat'
+import { mintUSDC, stealERC20 } from '../utils/hardhat'
 
 import {
   MyToken,
@@ -19,7 +19,8 @@ const {
   uniswapV3: { universalRouterAddress },
 } = networks[1]
 
-// const scenario = [USDC, WETH]
+const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+const MORPHO = '0x58D97B57BB95320F9a05dC918Aef65434969c2B2'
 
 describe('Swap and Deposit', () => {
   let relayPool: RelayPool
@@ -28,8 +29,8 @@ describe('Swap and Deposit', () => {
   let swapper: SwapAndDeposit
 
   before(async () => {
-    myToken = await ethers.deployContract('MyToken', ['My Token', 'TOKEN'])
-    expect(await myToken.totalSupply()).to.equal(1000000000000000000000000000n)
+    myToken = await ethers.getContractAt('MyToken', DAI)
+    expect(await myToken.symbol()).to.equal('DAI')
 
     // deploy 3rd party pool
     thirdPartyPool = await ethers.deployContract('MyYieldPool', [
@@ -61,13 +62,68 @@ describe('Swap and Deposit', () => {
     }))
   })
 
-  describe('USDC', () => {
-    const amount = ethers.parseUnits('100', 6)
+  it('has correct constructor params', async () => {
+    expect(await swapper.uniswapUniversalRouter()).to.equal(
+      universalRouterAddress
+    )
+  })
 
-    it('should swap any ERC20 token and deposit it directly into the pool', async () => {
-      const relayPoolAddress = await relayPool.getAddress()
+  describe('swap MORPHO correctly', () => {
+    const amount = ethers.parseUnits('100', 18)
+    let relayPoolAddress: string
+
+    before(async () => {
       const [user] = await ethers.getSigners()
       const userAddress = await user.getAddress()
+      relayPoolAddress = await relayPool.getAddress()
+
+      // get some USDC
+      const morpho = await ethers.getContractAt('IUSDC', MORPHO)
+      await stealERC20(
+        MORPHO,
+        '0x9D03bb2092270648d7480049d0E58d2FcF0E5123', // morpho whale
+        userAddress,
+        amount
+      )
+      // send some USDC to the pool
+      await morpho.connect(user).transfer(relayPoolAddress, amount)
+    })
+
+    it('should swap MORPHO into DAI and transfer it directly into the pool balance', async () => {
+      const balanceBefore = await getBalance(
+        relayPoolAddress,
+        MORPHO,
+        ethers.provider
+      )
+      expect(balanceBefore).to.be.equal(amount)
+      // set SwapDeposit contract
+      await relayPool.setSwapDeposit(await swapper.getAddress())
+      expect(await swapper.getAddress()).to.equal(
+        await relayPool.swapDepositAddress()
+      )
+
+      // swap that amount
+      await relayPool.swapAndDeposit(
+        MORPHO,
+        3000, // uniswapPoolFee
+        amount
+      )
+
+      // no usdc left
+      expect(
+        await getBalance(relayPoolAddress, MORPHO, ethers.provider)
+      ).to.be.equal(0)
+    })
+  })
+
+  describe('swap USDC correctly', () => {
+    const amount = ethers.parseUnits('100', 6)
+    let relayPoolAddress: string
+
+    before(async () => {
+      const [user] = await ethers.getSigners()
+      const userAddress = await user.getAddress()
+      relayPoolAddress = await relayPool.getAddress()
 
       // get some USDC
       await mintUSDC(USDC, userAddress, amount)
@@ -75,13 +131,15 @@ describe('Swap and Deposit', () => {
 
       // send some USDC to the pool
       await usdc.connect(user).transfer(relayPoolAddress, amount)
+    })
+
+    it('should swap USDC into DAI and transfer it directly into the pool balance', async () => {
       const balanceBefore = await getBalance(
         relayPoolAddress,
         USDC,
         ethers.provider
       )
       expect(balanceBefore).to.be.equal(amount)
-
       // set SwapDeposit contract
       await relayPool.setSwapDeposit(await swapper.getAddress())
       expect(await swapper.getAddress()).to.equal(
