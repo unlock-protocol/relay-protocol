a76bc4eab159adcac442eab60f84fc855bd33718
Julien Genestoux <julien.genestoux@gmail.com>
2025-01-28 09:34:21 -0500

    cleanup

--- backend/src/handlers/RelayBridgeFactory/BridgeDeployed.ts
+++ backend/src/handlers/RelayBridgeFactory/BridgeDeployed.ts
@@ -3,8 +3,6 @@ import { relayBridge } from 'ponder:schema'
 export default async function ({ event, context }) {
   const { bridge, asset } = event.args
 
-  console.log('GOT AN EVENT TO DEPLOY A BRIDGE!')
-
   await context.db.insert(relayBridge).values({
     chainId: context.network.chainId,
     contractAddress: bridge as `0x${string}`,
