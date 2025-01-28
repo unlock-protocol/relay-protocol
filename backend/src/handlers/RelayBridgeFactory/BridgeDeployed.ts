58111467995d1ab5e9ad4e6844c60dbde5fd090b
Julien Genestoux <julien.genestoux@gmail.com>
2025-01-28 09:34:21 -0500

    cleanup

--- backend/src/handlers/RelayBridgeFactory/BridgeDeployed.ts
+++ backend/src/handlers/RelayBridgeFactory/BridgeDeployed.ts
@@ -10,8 +10,6 @@ export default async function ({
 }) {
   const { bridge, asset } = event.args
 
-  console.log('GOT AN EVENT TO DEPLOY A BRIDGE!')
-
   await context.db.insert(relayBridge).values({
     chainId: context.network.chainId,
     contractAddress: bridge as `0x${string}`,
