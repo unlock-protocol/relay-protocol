906b2a2fe543778f055ad560f0459edfc6369a63
Clément Renaud <clement+git@clementrenaud.com>
2025-01-28 22:16:59 +0100

    feat: add abis package (#70)

---
+++ smart-contracts/tasks/utils/exportAbis.ts
@@ -0,0 +1,60 @@
+import fs from 'fs-extra'
+import semver from 'semver'
+import path from 'path'
+
+import { task } from 'hardhat/config'
+import { createIndexFile } from '@relay-protocol/helpers'
+
+const packageFolder = path.resolve('../packages/abis')
+
+const ignored = [
+  'hardhat/console.sol:console',
+  '@openzeppelin/contracts',
+  'contracts/utils',
+]
+
+task('export:abis', 'Export ABIs to a node package')
+  .addOptionalParam('previousVersion', 'version of the package to publish')
+  .setAction(async ({ previousVersion }, { ethers, artifacts }) => {
+    const version = semver.inc(previousVersion, 'patch')
+    console.log(`Releasing version : ${version}`)
+
+    // get only relevant files
+    const allContracts = await artifacts.getAllFullyQualifiedNames()
+    const startsWith = (name: string) =>
+      ignored.some((ign) => name.startsWith(ign))
+    const contractNames = allContracts.filter(
+      (qualifiedName) => !startsWith(qualifiedName)
+    )
+
+    // get artifacts
+    await Promise.all(
+      contractNames.map(async (qualifiedName) => {
+        const { abi, sourceName, contractName } =
+          await artifacts.readArtifact(qualifiedName)
+
+        const toExport = {
+          sourceName,
+          contractName,
+          abi: [new ethers.Interface(abi)].flat(),
+        }
+
+        const abiFileName = path.resolve(
+          packageFolder,
+          'src',
+          'abis',
+          sourceName.replace('contracts/', ''),
+          `${contractName}.json`
+        )
+
+        await fs.outputJSON(abiFileName, toExport.abi, { spaces: 2 })
+      })
+    )
+
+    // create repo files
+    await createIndexFile(
+      path.resolve(packageFolder, 'src', 'abis'),
+      path.resolve(packageFolder, 'src'),
+      path.resolve(packageFolder, 'src', 'abis')
+    )
+  })
