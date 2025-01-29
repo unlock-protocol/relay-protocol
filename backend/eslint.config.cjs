4cc002bf43e72e9a6f5cecb9e23bb5da46aa0c16
0xTxbi <techiejossy@gmail.com>
2025-01-29 12:31:34 +0100

    cleanup

--- backend/eslint.config.cjs
+++ backend/eslint.config.cjs
@@ -1,12 +1,3 @@
 const sharedConfig = require('@relay-protocol/eslint-config')
 
-// allow comments in JSON files
-const config = {
-  ...sharedConfig,
-  rules: {
-    ...sharedConfig.rules,
-    'json/*': ['error', { allowComments: true }],
-  },
-}
-
-module.exports = config
+module.exports = sharedConfig
