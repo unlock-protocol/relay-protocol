f5c6d470d665c59b8e315ccdc7fa76e291e81a95
0xTxbi <techiejossy@gmail.com>
2025-01-29 13:53:54 +0100

    update export util to suit ponder

--- smart-contracts/tasks/utils/exportAbis.ts
+++ smart-contracts/tasks/utils/exportAbis.ts
@@ -15,7 +15,7 @@ const ignored = [
 
 task('export:abis', 'Export ABIs to a node package')
   .addOptionalParam('previousVersion', 'version of the package to publish')
-  .setAction(async ({ previousVersion }, { ethers, artifacts }) => {
+  .setAction(async ({ previousVersion }, { artifacts }) => {
     const version = semver.inc(previousVersion, 'patch')
     console.log(`Releasing version : ${version}`)
 
@@ -36,7 +36,7 @@ task('export:abis', 'Export ABIs to a node package')
         const toExport = {
           sourceName,
           contractName,
-          abi: [new ethers.Interface(abi)].flat(),
+          abi,
         }
 
         const abiFileName = path.resolve(
@@ -44,17 +44,18 @@ task('export:abis', 'Export ABIs to a node package')
           'src',
           'abis',
           sourceName.replace('contracts/', ''),
-          `${contractName}.json`
+          `${contractName}.ts`
         )
 
-        await fs.outputJSON(abiFileName, toExport.abi, { spaces: 2 })
+        // export the abi as a constant (needed for ponder)
+        const abiContent = `export default ${JSON.stringify(toExport.abi, null, 2)} as const`
+        await fs.outputFile(abiFileName, abiContent)
       })
     )
 
     // create repo files
     await createIndexFile(
       path.resolve(packageFolder, 'src', 'abis'),
-      path.resolve(packageFolder, 'src'),
-      path.resolve(packageFolder, 'src', 'abis')
+      path.resolve(packageFolder, 'src')
     )
   })
