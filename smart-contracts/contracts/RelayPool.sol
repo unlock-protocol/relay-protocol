dfcd59fbb9dd646e133de79c30a490fedf3e3a25
Julien Genestoux <julien.genestoux@gmail.com>
2025-01-29 14:32:31 -0500

    updated comment

--- smart-contracts/contracts/RelayPool.sol
+++ smart-contracts/contracts/RelayPool.sol
@@ -236,9 +236,11 @@ contract RelayPool is ERC4626, Ownable {
   }
 
   // Helper function
-  // We deposit all the assets we own to the yield pool.
-  // This function can also be called by anyone if the pool owns
-  // tokens that are not in the yield pool.
+  // We deposit assets to the yield pool.
+  // This function is internal
+  // Note: a previous version used the full balance of assets.
+  //       This creates a vulnerability where a 3rd party can inflate
+  //       the share price and use that to capture the value created.
   function depositAssetsInYieldPool(uint amount) internal {
     ERC20(asset).approve(yieldPool, amount);
     ERC4626(yieldPool).deposit(amount, address(this));
