0aca38aa66cca0b833e142a71f86fcb02675b845
0xTxbi <techiejossy@gmail.com>
2025-01-30 21:15:13 +0100

    deduplicate events

--- backend/ponder.config.ts
+++ backend/ponder.config.ts
@@ -9,110 +9,6 @@ import {
 import { createNetworkConfig } from './src/utils/rpc'
 import { Abi, AbiEvent } from 'viem'
 
-const poolDeployedEvent: AbiEvent = {
-  anonymous: false,
-  inputs: [
-    {
-      indexed: true,
-      internalType: 'address',
-      name: 'pool',
-      type: 'address',
-    },
-    {
-      indexed: true,
-      internalType: 'address',
-      name: 'creator',
-      type: 'address',
-    },
-    {
-      indexed: true,
-      internalType: 'address',
-      name: 'asset',
-      type: 'address',
-    },
-    {
-      indexed: false,
-      internalType: 'string',
-      name: 'name',
-      type: 'string',
-    },
-    {
-      indexed: false,
-      internalType: 'string',
-      name: 'symbol',
-      type: 'string',
-    },
-    {
-      components: [
-        {
-          internalType: 'uint32',
-          name: 'chainId',
-          type: 'uint32',
-        },
-        {
-          internalType: 'address',
-          name: 'bridge',
-          type: 'address',
-        },
-        {
-          internalType: 'address',
-          name: 'proxyBridge',
-          type: 'address',
-        },
-        {
-          internalType: 'uint256',
-          name: 'maxDebt',
-          type: 'uint256',
-        },
-      ],
-      indexed: false,
-      internalType: 'struct OriginParam[]',
-      name: 'origins',
-      type: 'tuple[]',
-    },
-    {
-      indexed: false,
-      internalType: 'address',
-      name: 'thirdPartyPool',
-      type: 'address',
-    },
-    {
-      indexed: false,
-      internalType: 'uint8',
-      name: 'bridgeFee',
-      type: 'uint8',
-    },
-  ],
-  name: 'PoolDeployed',
-  type: 'event',
-}
-
-const bridgeDeployedEvent: AbiEvent = {
-  anonymous: false,
-  inputs: [
-    {
-      indexed: false,
-      internalType: 'address',
-      name: 'bridge',
-      type: 'address',
-    },
-    {
-      indexed: true,
-      internalType: 'address',
-      name: 'asset',
-      type: 'address',
-    },
-    {
-      indexed: true,
-      internalType: 'address',
-      name: 'proxyBridge',
-      type: 'address',
-    },
-  ],
-  name: 'BridgeDeployed',
-  type: 'event',
-}
-
 export default createConfig({
   database: {
     kind: 'postgres',
@@ -138,7 +34,9 @@ export default createConfig({
       network: 'sepolia',
       address: factory({
         address: '0xa6A1E3FE900282372a8c7d2a758aBAa1ba3EC7D5',
-        event: poolDeployedEvent,
+        event: RelayPoolFactory.find(
+          (e) => e.name === 'PoolDeployed'
+        ) as AbiEvent,
         parameter: 'pool',
       }),
       startBlock: 7499300,
@@ -169,7 +67,9 @@ export default createConfig({
               '0xacDFfaA0323440e123D68C25a073e99DeB82e3fC',
               '0x70e4A2b8de43459Fef02419A406158cD34A88dc3',
             ],
-            event: bridgeDeployedEvent,
+            event: RelayBridgeFactory.find(
+              (e) => e.name === 'BridgeDeployed'
+            ) as AbiEvent,
             parameter: 'bridge',
           }),
           startBlock: 7499300,
@@ -177,7 +77,9 @@ export default createConfig({
         baseSepolia: {
           address: factory({
             address: ['0xBB68C66467699faB205304810a1b288487F460d4'],
-            event: bridgeDeployedEvent,
+            event: RelayBridgeFactory.find(
+              (e) => e.name === 'BridgeDeployed'
+            ) as AbiEvent,
             parameter: 'bridge',
           }),
           startBlock: 20991910,
@@ -185,7 +87,9 @@ export default createConfig({
         arbSepolia: {
           address: factory({
             address: ['0x2499D94880B30fA505543550ac8a1e24cfFeFe78'],
-            event: bridgeDeployedEvent,
+            event: RelayBridgeFactory.find(
+              (e) => e.name === 'BridgeDeployed'
+            ) as AbiEvent,
             parameter: 'bridge',
           }),
           startBlock: 117524900,
