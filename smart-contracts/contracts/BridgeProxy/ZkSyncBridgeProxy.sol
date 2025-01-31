25ac1ec012bdcb1c126cf3bcbd46067307f0c296
Clément Renaud <clement+git@clementrenaud.com>
2025-01-31 16:19:36 +0100

    add claim decode logic

--- smart-contracts/contracts/BridgeProxy/ZkSyncBridgeProxy.sol
+++ smart-contracts/contracts/BridgeProxy/ZkSyncBridgeProxy.sol
@@ -6,6 +6,64 @@ import {IL1SharedBridge} from "../interfaces/zksync/IL1SharedBridge.sol";
 import {IL2SharedBridge} from "../interfaces/zksync/IL2SharedBridge.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+import "hardhat/console.sol";
+
+/**
+ * from https://github.com/matter-labs/era-contracts/blob/3288adb0aee6c1c3022f6c817f95234764e0d611/l1-contracts/contracts/common/libraries/UnsafeBytes.sol
+ * @author Matter Labs
+ * @custom:security-contact security@matterlabs.dev
+ * @dev The library provides a set of functions that help read data from an "abi.encodePacked" byte array.
+ * @dev Each of the functions accepts the `bytes memory` and the offset where data should be read and returns a value of a certain type.
+ *
+ * @dev WARNING!
+ * 1) Functions don't check the length of the bytes array, so it can go out of bounds.
+ * The user of the library must check for bytes length before using any functions from the library!
+ *
+ * 2) Read variables are not cleaned up - https://docs.soliditylang.org/en/v0.8.16/internals/variable_cleanup.html.
+ * Using data in inline assembly can lead to unexpected behavior!
+ */
+library UnsafeBytes {
+  function readUint32(
+    bytes memory _bytes,
+    uint256 _start
+  ) internal pure returns (uint32 result, uint256 offset) {
+    assembly {
+      offset := add(_start, 4)
+      result := mload(add(_bytes, offset))
+    }
+  }
+
+  function readAddress(
+    bytes memory _bytes,
+    uint256 _start
+  ) internal pure returns (address result, uint256 offset) {
+    assembly {
+      offset := add(_start, 20)
+      result := mload(add(_bytes, offset))
+    }
+  }
+
+  function readUint256(
+    bytes memory _bytes,
+    uint256 _start
+  ) internal pure returns (uint256 result, uint256 offset) {
+    assembly {
+      offset := add(_start, 32)
+      result := mload(add(_bytes, offset))
+    }
+  }
+
+  function readBytes32(
+    bytes memory _bytes,
+    uint256 _start
+  ) internal pure returns (bytes32 result, uint256 offset) {
+    assembly {
+      offset := add(_start, 32)
+      result := mload(add(_bytes, offset))
+    }
+  }
+}
+
 // docs
 // https://developers.circle.com/stablecoins/message-format
 contract ZkSyncBridgeProxy is BridgeProxy {
@@ -43,10 +101,16 @@ contract ZkSyncBridgeProxy is BridgeProxy {
     bytes32[] merkleProof;
   }
 
+  struct L2Message {
+    address sender;
+    bytes data;
+    uint256 txNumberInblock;
+  }
+
   function claim(
     address, // currency
     bytes calldata bridgeParams
-  ) external override returns (uint256) {
+  ) external override returns (uint256 amount) {
     // unpack args using assembly so we can keep it
     // as bytes 'calldata' to later decode the data
     // to finalize transfer in the outbox contract
@@ -56,7 +120,9 @@ contract ZkSyncBridgeProxy is BridgeProxy {
       params := bridgeParams.offset
     }
 
-    // TODO: decode value/amount from message
+    // decode value/amount from L2 > L1 message
+    amount = _parseL2WithdrawalMessage(params.message);
+    console.log(amount);
 
     // finalize withdrawal on L1
     L1_SHARED_BRIDGE.finalizeWithdrawal(
@@ -67,5 +133,57 @@ contract ZkSyncBridgeProxy is BridgeProxy {
       params.message,
       params.merkleProof
     );
+  }
+
+  // function below is used to parse the message
+  // and extract the amount of tokens claimed
+  // from: https://github.com/matter-labs/era-contracts/blob/3288adb0aee6c1c3022f6c817f95234764e0d611/l1-contracts/contracts/bridge/L1SharedBridge.sol#L763
+  error L2WithdrawalMessageWrongLength(uint256 messageLen);
+  error InvalidSelector(bytes4 func);
+
+  function _parseL2WithdrawalMessage(
+    bytes memory _l2ToL1message
+  ) internal pure returns (uint256 amount) {
+    // We check that the message is long enough to read the data.
+    // Please note that there are two versions of the message:
+    // 1. The message that is sent by `withdraw(address _l1Receiver)`
+    // It should be equal to the length of the bytes4 function signature + address l1Receiver + uint256 amount = 4 + 20 + 32 = 56 (bytes).
+    // 2. The message that is sent by `withdrawWithMessage(address _l1Receiver, bytes calldata _additionalData)`
+    // It should be equal to the length of the following:
+    // bytes4 function signature + address l1Receiver + uint256 amount + address l2Sender + bytes _additionalData =
+    // = 4 + 20 + 32 + 32 + _additionalData.length >= 68 (bytes).
+    // So the data is expected to be at least 56 bytes long.
+    // wrong message length
+    if (_l2ToL1message.length < 56) {
+      revert L2WithdrawalMessageWrongLength(_l2ToL1message.length);
+    }
+    (uint32 functionSignature, uint256 offset) = UnsafeBytes.readUint32(
+      _l2ToL1message,
+      0
+    );
+
+    // check for IMailbox.finalizeEthWithdrawal.selector
+    if (bytes4(functionSignature) == 0x6c0960f9) {
+      // this message is a base token withdrawal
+      (, offset) = UnsafeBytes.readAddress(_l2ToL1message, offset);
+      (amount, offset) = UnsafeBytes.readUint256(_l2ToL1message, offset);
+      // l1Token = BRIDGE_HUB.baseToken(_chainId);
+    } else if (
+      bytes4(functionSignature) == 0x11a2ccc1 // IL1ERC20Bridge.finalizeWithdrawal.selector
+    ) {
+      // We use the IL1ERC20Bridge for backward compatibility with old withdrawals.
+      // this message is a token withdrawal
+      // Check that the message length is correct.
+      // It should be equal to the length of the function signature + address + address + uint256 = 4 + 20 + 20 + 32 =
+      // 76 (bytes).
+      if (_l2ToL1message.length != 76) {
+        revert L2WithdrawalMessageWrongLength(_l2ToL1message.length);
+      }
+      (, offset) = UnsafeBytes.readAddress(_l2ToL1message, offset);
+      (, offset) = UnsafeBytes.readAddress(_l2ToL1message, offset);
+      (amount, offset) = UnsafeBytes.readUint256(_l2ToL1message, offset);
+    } else {
+      revert InvalidSelector(bytes4(functionSignature));
+    }
   }
 }
---
+++ smart-contracts/test/BridgeProxy/ZkSyncBridgeProxy.sepolia.ts
@@ -0,0 +1,126 @@
+import { ethers, ignition } from 'hardhat'
+import { AbiCoder, Interface } from 'ethers'
+import { expect } from 'chai'
+
+import { ZkSyncBridgeProxy } from '../../typechain-types'
+import { networks } from '@relay-protocol/networks'
+import { getEvent, getBalance } from '@relay-protocol/helpers'
+import { ERC20 as ERC20_ABI } from '@relay-protocol/helpers/abis'
+import ZkSyncBridgeProxyModule from '../../ignition/modules/ZkSyncBridgeProxyModule'
+
+const ETH_CHAIN_ID = 11155111n
+const ZKSYNC_CHAIN_ID = 300n
+const {
+  bridges: {
+    zksync: { l2SharedDefaultBridge, l1SharedDefaultBridge },
+  },
+  assets: { weth: USDC },
+} = networks[ETH_CHAIN_ID.toString()]
+
+// USDC bridge tx on Sepolia
+// https://sepolia.arbiscan.io/tx/0x075f42ae6e150209c543959ca8b0f1e3bedcf146c86fb19a073f783b38ed15f3
+const recipientAddress = '0x246A13358Fb27523642D86367a51C2aEB137Ac6C'
+const amount = 1000n
+
+// construct the actual proof
+const returnedProof = {
+  l1BatchNumber: 13523,
+  l2MessageIndex: 6,
+  l2TxNumberInBlock: 240,
+  message:
+    '0x11a2ccc1246a13358fb27523642d86367a51c2aeb137ac6c1c7d4b196cb0c7b01d743fbc6116a902379c723800000000000000000000000000000000000000000000000000000000000003e8',
+  sender: '0x681A1AFdC2e06776816386500D2D461a6C96cB45',
+  proof: [
+    '0x2bfb4d4babd077f13ab7b723da6266962f6977c7d02be162c6f8afb67e0bf6d7',
+    '0x1edd6057bcbda3e1e1cfd2c845644248272f0cb3255541eb9ec63ee6fd89f46e',
+    '0x80f27360c37f8896f3d29ca5ccef8da115ec7d613ef8d8b624705df61a097dd7',
+    '0x60d24c3ddd85d8e7ed957465a52bd9e1e4d9d26b72c6fb50dfc7a6e24e28dbb1',
+    '0xe4733f281f18ba3ea8775dd62d2fcd84011c8c938f16ea5790fd29a03bf8db89',
+    '0x1798a1fd9c8fbb818c98cff190daa7cc10b6e5ac9716b4a2649f7c2ebcef2272',
+    '0x66d7c5983afe44cf15ea8cf565b34c6c31ff0cb4dd744524f7842b942d08770d',
+    '0xb04e5ee349086985f74b73971ce9dfe76bbed95c84906c5dffd96504e1e5396c',
+    '0xac506ecb5465659b3a927143f6d724f91d8d9c4bdb2463aee111d9aa869874db',
+    '0x124b05ec272cecd7538fdafe53b6628d31188ffb6f345139aac3c3c1fd2e470f',
+    '0xc3be9cbd19304d84cca3d045e06b8db3acd68c304fc9cd4cbffe6d18036cb13f',
+    '0xfef7bd9f889811e59e4076a0174087135f080177302763019adaf531257e3a87',
+    '0xa707d1c62d8be699d34cb74804fdd7b4c568b6c1a821066f126c680d4b83e00b',
+    '0xf6e093070e0389d2e529d60fadb855fdded54976ec50ac709e3a36ceaa64c291',
+  ],
+}
+
+// encode args to pass to pool
+const args = [
+  ZKSYNC_CHAIN_ID,
+  returnedProof.l1BatchNumber,
+  returnedProof.l2MessageIndex,
+  returnedProof.l2TxNumberInBlock,
+  returnedProof.message,
+  returnedProof.proof,
+]
+
+// send claim to the pool
+const abiCoder = new AbiCoder()
+const bridgeParams = abiCoder.encode(
+  ['uint256', 'uint256', 'uint256', 'uint16', 'bytes', 'bytes32[]'],
+  args
+)
+
+describe('ZkSyncBridgeProxy', function () {
+  let bridge: ZkSyncBridgeProxy
+
+  before(async () => {
+    // deploy using ignition
+    const parameters = {
+      ZkSyncBridgeProxy: {
+        l2SharedDefaultBridge,
+        l1SharedDefaultBridge,
+      },
+    }
+
+    ;({ bridge } = await ignition.deploy(ZkSyncBridgeProxyModule, {
+      parameters,
+    }))
+  })
+
+  it('constructor values are correct', async () => {
+    expect(await bridge.L2_SHARED_BRIDGE()).to.be.equal(l2SharedDefaultBridge)
+    expect(await bridge.L1_SHARED_BRIDGE()).to.be.equal(l1SharedDefaultBridge)
+  })
+
+  describe('claim using BridgeProxy directly', () => {
+    it('works with ERC20 (USDC)', async () => {
+      const balanceBefore = await getBalance(
+        recipientAddress,
+        USDC!,
+        ethers.provider
+      )
+      const tx = await bridge.claim(ethers.ZeroAddress, bridgeParams)
+      const balanceAfter = await getBalance(
+        recipientAddress,
+        USDC!,
+        ethers.provider
+      )
+
+      const receipt = await tx.wait()
+      console.log(receipt)
+
+      // weth transfer happened
+      const { event: erc20TransferEvent } = await getEvent(
+        receipt!,
+        'Transfer',
+        new Interface(ERC20_ABI)
+      )
+      expect(erc20TransferEvent.args.to).to.equals(recipientAddress)
+      expect(erc20TransferEvent.args.amount).to.equals(amount.toString())
+
+      // // outbox recorded
+      // const { event: outboxEvent } = await getEvent(
+      //   receipt!,
+      //   'OutBoxTransactionExecuted'
+      // )
+      // expect(outboxEvent.args.to).to.equals(returnedProof.destination)
+      // expect(outboxEvent.args.l2Sender).to.equals(returnedProof.caller)
+      // expect(balanceAfter).to.equals(balanceBefore + amount)
+    })
+  })
+})
