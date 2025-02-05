e549cd4636534a0fbc413b86b33c6a2927ca3b32
Julien Genestoux <julien.genestoux@gmail.com>
2025-02-05 17:08:46 -0500

    undue changes

--- smart-contracts/tasks/networks.ts
+++
@@ -1,9 +0,0 @@
-import { task } from 'hardhat/config'
-
-import { networks } from '@relay-protocol/networks'
-
-task('networks:list', 'List all suported networks').setAction(async () => {
-  Object.values(networks).forEach((network) => {
-    console.log(`${network.name} (${network.chainId} ${network.slug})`)
-  })
-})
