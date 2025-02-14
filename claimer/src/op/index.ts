5f08f3e9130665c7d7e57a10f4b5c541375a45da
Julien Genestoux <julien.genestoux@gmail.com>
2025-02-14 09:42:59 -0500

    so far so good

--- claimer/src/op/index.ts
+++ claimer/src/op/index.ts
@@ -1,8 +1,12 @@
-import { buildProveWithdrawal } from '@relay-protocol/helpers'
+import {
+  buildFinalizeWithdrawal,
+  buildProveWithdrawal,
+} from '@relay-protocol/helpers'
 import { Portal2 } from '@relay-protocol/helpers/abis'
+import * as ABIs from '@relay-protocol/abis'
 
 import networks from '@relay-protocol/networks'
-import { ethers } from 'ethers'
+import { AbiCoder, ethers } from 'ethers'
 import { getSignerForNetwork } from '../signer'
 
 export const submitProof = async ({
@@ -35,11 +39,29 @@ export const submitProof = async ({
 }
 
 export const claimWithdrawal = async (bridgeTransaction) => {
-  console.log('OP Claim me!')
-  console.log(bridgeTransaction)
-  const originNetwork = networks[bridgeTransaction.originChainId]
+  // console.log('OP Claim me!')
+  // console.log(bridgeTransaction)
   const destinationNetwork = networks[bridgeTransaction.destinationPoolChainId]
-  // const pool = new ethers.Contract()
+  // Get the relaypool contract
+  const signer = await getSignerForNetwork(destinationNetwork)
+  const relayPool = new ethers.Contract(
+    bridgeTransaction.destinationPoolAddress,
+    ABIs.RelayPool,
+    signer
+  )
+
+  const finalizeParams = await buildFinalizeWithdrawal(
+    10,
+    '0x8a8ed32ec52267ba5c5656dc68f459a8be3cdd23d8a1128ed321a2c6df2e8ee3'
+  )
+  const claimParams = new AbiCoder().encode(
+    ['bytes', 'address'],
+    [finalizeParams, submitter] // Hum, we need to the submitted address (it should be us though)
+  )
+
+  await relayPool.claim(bridgeTransaction.originTxHash)
+  // Send `claim` transaction!
+  // And that's it~
 }
 
 export default {
