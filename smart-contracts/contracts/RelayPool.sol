24dc8399e7a83198e71899061be6ab61b27ef4c1
Julien Genestoux <julien.genestoux@gmail.com>
2025-02-17 18:17:16 -0500

    outstanding debt events include origin (#168)
    * outstanding debt events include origin
    
    * updated event
    
    * fixed tests

--- smart-contracts/contracts/RelayPool.sol
+++ smart-contracts/contracts/RelayPool.sol
@@ -105,7 +105,13 @@ contract RelayPool is ERC4626, Ownable {
     bytes claimParams
   );
 
-  event OutstandingDebtChanged(uint256 oldDebt, uint256 newDebt);
+  event OutstandingDebtChanged(
+    uint256 oldDebt,
+    uint256 newDebt,
+    OriginSettings origin,
+    uint256 oldOriginDebt,
+    uint256 newOriginDebt
+  );
 
   event AssetsDepositedIntoYieldPool(uint256 amount, address yieldPool);
   event AssetsWithdrawnFromYieldPool(uint256 amount, address yieldPool);
@@ -205,16 +211,38 @@ contract RelayPool is ERC4626, Ownable {
     );
   }
 
-  function increaseOutStandingDebt(uint256 amount) internal {
+  function increaseOutStandingDebt(
+    uint256 amount,
+    OriginSettings storage origin
+  ) internal {
+    uint256 currentOriginOutstandingDebt = origin.outstandingDebt;
+    origin.outstandingDebt += amount;
     uint256 currentOutstandingDebt = outstandingDebt;
     outstandingDebt += amount;
-    emit OutstandingDebtChanged(currentOutstandingDebt, outstandingDebt);
+    emit OutstandingDebtChanged(
+      currentOutstandingDebt,
+      outstandingDebt,
+      origin,
+      currentOriginOutstandingDebt,
+      origin.outstandingDebt
+    );
   }
 
-  function decreaseOutStandingDebt(uint256 amount) internal {
+  function decreaseOutStandingDebt(
+    uint256 amount,
+    OriginSettings storage origin
+  ) internal {
+    uint256 currentOriginOutstandingDebt = origin.outstandingDebt;
+    origin.outstandingDebt -= amount;
     uint256 currentOutstandingDebt = outstandingDebt;
     outstandingDebt -= amount;
-    emit OutstandingDebtChanged(currentOutstandingDebt, outstandingDebt);
+    emit OutstandingDebtChanged(
+      currentOutstandingDebt,
+      outstandingDebt,
+      origin,
+      currentOriginOutstandingDebt,
+      origin.outstandingDebt
+    );
   }
 
   // We cap the maxDeposit of any receiver to the maxDeposit of the yield pool for us
@@ -358,8 +386,7 @@ contract RelayPool is ERC4626, Ownable {
         message.amount
       );
     }
-    origin.outstandingDebt += message.amount;
-    increaseOutStandingDebt(message.amount);
+    increaseOutStandingDebt(message.amount, origin);
 
     // We only send the amount net of fees
     sendFunds(message.amount - feeAmount, message.recipient);
@@ -436,9 +463,7 @@ contract RelayPool is ERC4626, Ownable {
     uint256 amount = abi.decode(result, (uint256));
 
     // We should have received funds
-    // Update the outstanding debts (both for the origin and the pool total)
-    origin.outstandingDebt -= amount;
-    decreaseOutStandingDebt(amount);
+    decreaseOutStandingDebt(amount, origin);
     // and we should deposit these funds into the yield pool
     depositAssetsInYieldPool(amount);
 
