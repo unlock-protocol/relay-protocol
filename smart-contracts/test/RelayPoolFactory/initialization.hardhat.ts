63dc421e958c56fbe66769076bb004309465b458
Julien Genestoux <julien.genestoux@gmail.com>
2025-02-18 17:02:47 -0500

    fixed tests

--- smart-contracts/test/RelayPoolFactory/initialization.hardhat.ts
+++ smart-contracts/test/RelayPoolFactory/initialization.hardhat.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 import { ethers, ignition } from 'hardhat'
-import { MyToken, RelayPoolFactory } from '../../typechain-types'
+import { MyToken, MyYieldPool, RelayPoolFactory } from '../../typechain-types'
 import RelayPoolFactoryModule from '../../ignition/modules/RelayPoolFactoryModule'
 import { getEvent } from '@relay-protocol/helpers'
 
@@ -10,10 +10,16 @@ describe('RelayPoolFactory: deployment', () => {
   let timelockTemplate: any
   const hyperlaneMailbox = '0x1000000000000000000000000000000000000000'
   const weth = '0x2000000000000000000000000000000000000000'
-  const thirdPartyPool = '0x3000000000000000000000000000000000000000'
+  let thirdPartyPool: MyYieldPool
 
   before(async () => {
     const { chainId } = await ethers.provider.getNetwork()
+
+    thirdPartyPool = await ethers.deployContract('MyYieldPool', [
+      await myToken.getAddress(),
+      'My Yield Pool',
+      'YIELD',
+    ])
 
     // Deploy an "empty" timelock
     const TimelockController = await ethers.getContractFactory(
@@ -48,13 +54,16 @@ describe('RelayPoolFactory: deployment', () => {
   it('should let user deploy a pool', async () => {
     const [user] = await ethers.getSigners()
     const userAddress = await user.getAddress()
+    const initialDeposit = ethers.parseUnits('10', await myToken.decimals())
+    await myToken.approve(await relayPoolFactory.getAddress(), initialDeposit)
     const tx = await relayPoolFactory.deployPool(
       await myToken.getAddress(),
       'Test Vault',
       'RELAY',
       [],
-      thirdPartyPool,
-      60 * 60 * 24 * 7
+      await thirdPartyPool.getAddress(),
+      60 * 60 * 24 * 7,
+      initialDeposit
     )
     const receipt = await tx.wait()
     const event = await getEvent(
